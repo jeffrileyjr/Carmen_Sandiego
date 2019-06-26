fac593b667ca60027f7e6f7d42606f67c9d7a107
jeff <jeffrileyjr80@live.com>
2019-06-25 22:06:57 -0400

    trying to fix heroku build error

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -10,25 +10,25 @@ import {
 } from '@angular/animations';
 
 
-export const fader =
-  trigger('routeAnimations', [
-    transition('* <=> *', [
-      // Set a default  style for enter and leave
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          left: 0,
-          width: '100%',
-          opacity: 0,
-          transform: 'scale(0) translateY(100%)',
-        }),
-      ]),
-      // Animate the new page in
-      query(':enter', [
-        animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' })),
-      ])
-    ]),
-]);
+// export const fader =
+//   trigger('routeAnimations', [
+//     transition('* <=> *', [
+//       // Set a default  style for enter and leave
+//       query(':enter, :leave', [
+//         style({
+//           position: 'absolute',
+//           left: 0,
+//           width: '100%',
+//           opacity: 0,
+//           transform: 'scale(0) translateY(100%)',
+//         }),
+//       ]),
+//       // Animate the new page in
+//       query(':enter', [
+//         animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' })),
+//       ])
+//     ]),
+// ]);
 
 export const slider =
   trigger('routeAnimations', [
@@ -67,36 +67,38 @@ function slideTo(direction) {
     // query(':enter', animateChild()),
   ];
 }
-export const transformer =
-  trigger('routeAnimations', [
-    transition('* => isLeft', transformTo({ x: -100, y: -100, rotate: -720 }) ),
-    transition('* => isRight', transformTo({ x: 100, y: -100, rotate: 90 }) ),
-    transition('isRight => *', transformTo({ x: -100, y: -100, rotate: 360 }) ),
-    transition('isLeft => *', transformTo({ x: 100, y: -100, rotate: -360 }) )
-]);
+
+export {slideTo}
+// export const transformer =
+//   trigger('routeAnimations', [
+//     transition('* => isLeft', transformTo({ x: -100, y: -100, rotate: -720 }) ),
+//     transition('* => isRight', transformTo({ x: 100, y: -100, rotate: 90 }) ),
+//     transition('isRight => *', transformTo({ x: -100, y: -100, rotate: 360 }) ),
+//     transition('isLeft => *', transformTo({ x: 100, y: -100, rotate: -360 }) )
+// ]);
 
 
-function transformTo({x = 100, y = 0, rotate = 0}) {
-  const optional = { optional: true };
-  return [
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%'
-      })
-    ], optional),
-    query(':enter', [
-      style({ transform: `translate(${x}%, ${y}%) rotate(${rotate}deg)`})
-    ]),
-    group([
-      query(':leave', [
-        animate('700ms ease-out', style({ transform: `translate(${x}%, ${y}%) rotate(${rotate}deg)`}))
-      ], optional),
-      query(':enter', [
-        animate('700ms ease-out', style({ transform: `translate(0, 0) rotate(0)`}))
-      ])
-    ]),
-  ];
-}
+// function transformTo({x = 100, y = 0, rotate = 0}) {
+//   const optional = { optional: true };
+//   return [
+//     query(':enter, :leave', [
+//       style({
+//         position: 'absolute',
+//         top: 0,
+//         left: 0,
+//         width: '100%'
+//       })
+//     ], optional),
+//     query(':enter', [
+//       style({ transform: `translate(${x}%, ${y}%) rotate(${rotate}deg)`})
+//     ]),
+//     group([
+//       query(':leave', [
+//         animate('700ms ease-out', style({ transform: `translate(${x}%, ${y}%) rotate(${rotate}deg)`}))
+//       ], optional),
+//       query(':enter', [
+//         animate('700ms ease-out', style({ transform: `translate(0, 0) rotate(0)`}))
+//       ])
+//     ]),
+//   ];
+// }
