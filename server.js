2d91d95c936f1c1c195caaefe3d9ca0019d5c96b
jeff <jeffrileyjr80@live.com>
2019-06-25 21:28:10 -0400

    setting up heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const clues = require('./clues.api');
 const stolen = require('./stolen.api');
 const scores = require("./scores.api"); 
 app.use(cors());
-app.use(express.static(__dirname + "/public")); 
+app.use(express.static(__dirname + "/dist")); 
 app.use(express.json()); 
 app.use("/", clues); 
 app.use("/", stolen);
 app.use("/", scores);
 
-let port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on PORT: ${port}.`));
