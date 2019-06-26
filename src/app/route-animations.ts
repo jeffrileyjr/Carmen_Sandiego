c7f166426ae9b4f1e0ea301211d6fd5fd254bde4
jeff <jeffrileyjr80@live.com>
2019-06-25 23:19:05 -0400

    trying to fix heroku build error due to router animations

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -30,43 +30,58 @@ export const fader =
     ]),
 ]);
 
+const optional = { optional: true };
+let toTheRight = [
+  query(':enter, :leave', [
+    style({
+      position: 'absolute',
+      top: '0',
+      right: 0,
+      width: '100%',
+    })
+  ], optional),
+  query(':enter', [
+    style({ right: '-100%',  })
+  ]),
+  group([
+    query(':leave', [
+      animate('300ms ease', style({ right: '100%', }))
+    ], optional),
+    query(':enter', [
+      animate('300ms ease', style({ right: '0%'}))
+    ])
+  ]),
+];
+let toTheLeft = [
+  query(':enter, :leave', [
+    style({
+      position: 'absolute',
+      top: '0',
+      left: 0,
+      width: '100%',
+    })
+  ], optional),
+  query(':enter', [
+    style({ right: '-100%',  })
+  ]),
+  group([
+    query(':leave', [
+      animate('300ms ease', style({ left: '100%', }))
+    ], optional),
+    query(':enter', [
+      animate('300ms ease', style({ left: '0%'}))
+    ])
+  ]),
+]
+ 
+ 
 export const slider =
   trigger('routeAnimations', [
-    transition('* => isLeft', slideTo('left') ),
-    transition('* => isRight', slideTo('right') ),
-    transition('isRight => *', slideTo('left') ),
-    transition('isLeft => *', slideTo('right') )
+    transition('* => isLeft', toTheLeft),
+    transition('* => isRight', toTheRight),
+    transition('isRight => *', toTheLeft),
+    transition('isLeft => *', toTheRight),
   ]);
-
-export function slideTo(direction) {
-  // const optional = { optional: true };
-  return [
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        [direction]: 0,
-        width: '100%'
-      })
-    ]),
-    query(':enter', [
-      style({ [direction]: '-100%'})
-    ]),
-    group([
-      query(':leave', [
-        animate('800ms ease', style({ [direction]: '100%'}))
-      ]),
-      query(':enter', [
-        animate('800ms ease', style({ [direction]: '0%'}))
-      ])
-    ]),
-    // Normalize the page style... Might not be necessary
-
-    // Required only if you have child animations on the page
-    // query(':leave', animateChild()),
-    // query(':enter', animateChild()),
-  ];
-}
 
 // export const transformer =
 //   trigger('routeAnimations', [
